2bcc0f72ca14ba908bcfe7d4b186b38713e40653
glendell03 <dellbringino@gmail.com>
2021-01-18 16:55:29 +0800

    Added Landing, Team, Projects, Contact US

---
+++ src/components/pages/ContactUs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactUs = () => {
+  return <div className="h-screen flex items-center justify-center">Contact Us</div>;
+};
+
+export default ContactUs;
---
+++ src/components/pages/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div className="h-screen flex items-center justify-center">Projects</div>;
+};
+
+export default Projects;
