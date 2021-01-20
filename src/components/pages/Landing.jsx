66cc63aab5e37c2e040d71468be77becabc54bfd
ravenlntn <rgdlantin@iskolarngbayan.pup.edu.ph>
2021-01-20 22:42:05 +0800

    updated landing page

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as Logo } from "../../assets/img/Alt-f4-New-Logo.svg";
 
 const Landing = () => {
   return (
-    
+
     <div className="h-screen flex relative items-center">
       <BlobsLanding className="absolute right-0 bottom-0 h-full w-2/4"/>
 
@@ -17,6 +17,7 @@ const Landing = () => {
       <div>
         <p className="font-jura text-2xl">Student Developers from</p>
         <p className="font-jura text-2xl">Polytechnic University of the Philippines - Manila</p>
+        <p className="font-jura text-2xl">Delete mo nalang HAHAAH </p>
       </div>      
     </div>
 
