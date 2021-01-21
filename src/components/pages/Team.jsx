cdbaf9099b238e68b576a7dcc5c92541ec69fc05
ravenlntn <rgdlantin@iskolarngbayan.pup.edu.ph>
2021-01-21 17:46:48 +0800

    desc

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Team = () => {
-  return <div className="h-screen flex items-center justify-center">Team</div>;
+  return <div className="h-screen flex items-center justify-center">Team </div>;
 };
 
 export default Team;
