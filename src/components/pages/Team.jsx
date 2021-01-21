5806588189089689b795deb94a3b05c74a387485
glendell03 <dellbringino@gmail.com>
2021-01-21 17:36:16 +0800

    Fix blobs problem

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import BlobsTeam from "../../assets/img/blobs-team.svg";
+
 const Team = () => {
-  return <div className="h-screen flex items-center justify-center">Team</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <img
+        className="h-full absolute -right-8"
+        src={BlobsTeam}
+        alt="BlobsTeam"
+      />
+    </div>
+  );
 };
 
 export default Team;
