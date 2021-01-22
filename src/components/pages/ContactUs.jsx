9904264bdd455597c4060c876825a7ae19aa2eaf
ravenlntn <rgdlantin@iskolarngbayan.pup.edu.ph>
2021-01-22 17:53:02 +0800

    contacts + adjust konti ng up + view projects

--- src/components/pages/ContactUs.jsx
+++ src/components/pages/ContactUs.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import NavigationText from "../layout/NavigationText";
 import { ReactComponent as DotGrid } from "../../assets/img/DotGrid.svg";
+import BlobsContacts from "../../assets/img/contacts-blobs.svg";
 
 const ContactUs = () => {
   return (
     <div className="h-screen flex items-center">
       <NavigationText navText={"CONTACT US"} />
-      Contact Us
 
+      <img
+        className="h-full absolute right-0"
+        src={BlobsContacts}
+        alt="BlobsContacts"
+      />
+      
       <DotGrid className="absolute bottom-20 left-14" />
 
     </div>
