10739e8c5edfdb301dd74d3436d92433f3ef2d2c
glendell03 <dellbringino@gmail.com>
2021-03-04 19:24:29 +0800

    fix teamcard alignment

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -4,14 +4,12 @@ import TeamCard from "../layout/TeamCard";
 
 const Team = () => {
   return (
-    <div className="h-full mb-72 pb-72 md:mb-0 md:pb-0 container mx-auto flex flex-col items-center justify-center">
-      <h1 className="font-majorMono text-4xl sm:text-7xl xl:text-9xl flex items-center">
+    <div className="h-full mb-72 pb-72 md:mb-0 md:pb-0 container mx-auto flex flex-col items-center justify-start lg:justify-center">
+      <h1 className="font-majorMono text-4xl sm:text-7xl xl:text-9xl flex items-center mt-28 mb-40">
         OUR TEAM
       </h1>
 
-      <div className="mt-40 mb-40 xl:mb-0">
-        <TeamCard />
-      </div>
+      <TeamCard />
     </div>
   );
 };
