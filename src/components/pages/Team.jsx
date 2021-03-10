bc37c0b4a798cf345a6b5304a8923f7e547307ca
glendell03 <dellbringino@gmail.com>
2021-03-10 17:06:22 +0800

    New Home UI

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -4,7 +4,7 @@ import TeamCard from "../layout/TeamCard";
 const Team = () => {
   return (
     <div className="h-screen container mx-auto flex flex-col items-center md:justify-center">
-      <div className="mt-16 md:mt-0">
+      <div className="mt-16 md:mt-96 lg:mt-56 xl:mt-0">
         <h1 className="font-righteous text-2xl text-center mx-8 py-10 md:text-4xl lg:text-5xl xl:text-6xl">
           MEET THE CREWMATES
         </h1>
