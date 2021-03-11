745ecc32cd1f3d68bcf955b57b103566eae7454a
ravenlntn <rgdlantin@iskolarngbayan.pup.edu.ph>
2021-03-11 21:20:03 +0800

    font size

--- src/components/layout/ServicesCard.jsx
+++ src/components/layout/ServicesCard.jsx
@@ -6,7 +6,7 @@ const ServicesCard = ({ text, bgPallete, children }) => {
     <div className="card h-96 w-72 relative">
       <div className="z-10 absolute w-full h-full flex flex-col items-center">
         {children}
-        <p className="flex-1 font-righteous tracking-widest">{text}</p>
+        <p className="flex-1 font-righteous tracking-widest text-xl">{text}</p>
       </div>
       <div className={`liquid ${bgPallete}`}></div>
     </div>
